78bc5e2b25e703d052066a98b4a9f75cfbda46c5
Dima Dorezyuk <dorezyuk@magazino.eu>
2021-04-25 11:33:40 +0200

    move core parts of transform_tree into _transform_tree

--- src/fast_tf.cpp
+++ src/fast_tf.cpp
@@ -46,6 +46,11 @@ static_transform::get() const noexcept {
   return data_;
 }
 
+counter::scoped_signal::scoped_signal(const counter& _c) :
+    counter_(++_c.counter_) {}
+
+counter::scoped_signal::~scoped_signal() { --counter_; }
+
 dynamic_transform::dynamic_transform() noexcept :
     dur_(std::chrono::duration_cast<duration_t>(
         std::chrono::milliseconds(500))) {}
@@ -123,12 +128,13 @@ dynamic_transform::get(const time_t& _time) const {
   }
 }
 
-transform_tree::_leaf::_leaf(const std::string& _frame_id,
-                             variable_transform&& _data) noexcept :
+_transform_tree::_leaf::_leaf(const std::string& _frame_id,
+                              variable_transform&& _data) noexcept :
     frame_id(_frame_id), data(std::move(_data)) {}
 
-transform_tree::_leaf::_leaf(const std::string& _frame_id, const _leaf& _parent,
-                             variable_transform&& _data) noexcept :
+_transform_tree::_leaf::_leaf(const std::string& _frame_id,
+                              const _leaf& _parent,
+                              variable_transform&& _data) noexcept :
     frame_id(_frame_id),
     depth(_parent.depth + 1),
     parent(&_parent),
@@ -142,6 +148,115 @@ sequence_factory(bool _static) noexcept {
     return static_transform{};
   else
     return dynamic_transform{};
+}
+
+_transform_tree::iterator
+_transform_tree::init(const std::string& _parent_frame,
+                      const std::string& _child_frame) {
+  if (!empty()) {
+    // check if the child-frame-id is the current root. if not - we must reject
+    // the init-call since we already have a valid root-node.
+    assert(root_ != data_.end() && "root cannot be data_.end()");
+    if (root_->first != _child_frame)
+      throw std::runtime_error("cannot reinit the map");
+
+    // increment the map's depth counters
+    for (auto& n : data_)
+      ++n.second.depth;
+
+    // create the root node and wire up the connections. note: the root node is
+    // always static.
+    const auto res =
+        data_.emplace(_parent_frame, _leaf{_parent_frame, static_transform{}});
+
+    assert(res.second && "failed to create the root node");
+
+    root_->second.parent = &res.first->second;
+    root_ = res.first;
+  }
+  else {
+    const auto res =
+        data_.emplace(_parent_frame, _leaf{_parent_frame, static_transform{}});
+
+    assert(res.second && "failed to create the root node");
+
+    root_ = res.first;
+  }
+
+  assert(!empty() && "tree cannot be empty when leaving init()");
+  return root_;
+}
+
+_transform_tree::iterator
+_transform_tree::emplace(const std::string& _parent_frame,
+                         const std::string& _child_frame, bool _is_static) {
+  // check if one of the inputs in empty
+  if (_parent_frame.empty() || _child_frame.empty())
+    throw std::runtime_error("empty frame detected");
+
+  // check if the input is just self-transform
+  if (_parent_frame == _child_frame)
+    throw std::runtime_error("self-transform detected");
+
+  auto parent = data_.find(_parent_frame);
+
+  // if the parent is unknown we try to add it to the tree a the new root.
+  // this only works if the tree is either empty or if the child link is the
+  // current root. in all other cases we will throw.
+  if (parent == data_.end())
+    parent = init(_parent_frame, _child_frame);
+
+  // check if the child is known
+  auto child = data_.find(_child_frame);
+  if (child == data_.end()) {
+    // create a new node
+    auto res = data_.emplace(_child_frame, _leaf{_child_frame, parent->second,
+                                                 sequence_factory(_is_static)});
+    assert(res.second && "failed to insert new child");
+    child = res.first;
+  }
+  else {
+    // make sure that the newly added link is consistent. this will also
+    // prevent us from creating cyclic graphs.
+    if (child->second.parent != &parent->second)
+      throw std::runtime_error("child cannot have multiple parents");
+  }
+  return child;
+}
+
+void
+_transform_tree::merge(_transform_tree& _other) {
+  // calling this on an empty tree is a noop.
+  if (_other.empty() || empty())
+    return;
+
+  // cn denotes the connecting link between the trees. the syntax below works
+  // only with c++17.
+  impl_t::const_iterator cn;
+  if (cn = data_.find(_other.root_->first); cn != data_.end()) {
+    // merge other into this
+    for (auto& p : _other.data_)
+      p.second.depth += cn->second.depth;
+  }
+  else if (cn = _other.data_.find(root_->first); cn != _other.data_.end()) {
+    // merge this into other
+    for (auto& p : data_)
+      p.second.depth += cn->second.depth;
+  }
+  else
+    throw std::runtime_error("cannot merge unconnected trees");
+
+  // rewire the pointers to the root
+  for (auto& p : _other.data_)
+    if (p.second.parent == &_other.root_->second)
+      p.second.parent = &root_->second;
+
+  // pass over the members from _other to this.
+  data_.merge(_other.data_);
+
+  // only root shall remain in the _other since this is the connecting link.
+  assert(_other.size() == 1 && "duplicated links detected");
+  _other.data_.clear();
 }
 
 /// @brief conversion from ros::Time to std::chrono.
@@ -159,10 +274,14 @@ to_duration(const ros::Duration& _dur) {
          duration_cast<duration_t>(nanoseconds(_dur.nsec));
 }
 
+static Eigen::Isometry3d
+get_transform(dynamic_transform& _dt, const time_t& _time,
+              const time_t _end_time) {}
+
 /// @brief applies the transform stored in _l on _r.
 static Eigen::Isometry3d
 chain_transform(const variable_transform& _l, const Eigen::Isometry3d& _r,
-                const time_t& _time) noexcept {
+                const time_t& _time) {
   if (std::holds_alternative<static_transform>(_l))
     return std::get<static_transform>(_l).get() * _r;
   else
@@ -171,61 +290,36 @@ chain_transform(const variable_transform& _l, const Eigen::Isometry3d& _r,
 
 void
 transform_tree::add(const geometry_msgs::TransformStamped& _tf, bool _static) {
-  // check if one of the inputs in empty
-  if (_tf.child_frame_id.empty() || _tf.header.frame_id.empty())
-    throw std::runtime_error("empty frame detected");
-
-  // check if the input is just self-transform
-  if (_tf.child_frame_id == _tf.header.frame_id)
-    throw std::runtime_error("self-transform detected");
-
-  // check if the parent is known
-  auto parent = data_.find(_tf.header.frame_id);
-
-  // if the parent is unknown we try to add it to the tree a the new root. this
-  // only works if the tree is either empty or if the child link is the current
-  // root. in all other cases we will throw.
-  if (parent == data_.end())
-    parent = init(_tf);
-
-  // check if the child is known
-  auto child = data_.find(_tf.child_frame_id);
-  if (child == data_.end()) {
-    // create a new node
-    auto res = data_.emplace(
-        _tf.child_frame_id,
-        _leaf{_tf.child_frame_id, parent->second, sequence_factory(_static)});
-    assert(res.second && "failed to insert new child");
-    child = res.first;
-  }
-  else {
-    // make sure that the newly added link is consistent. this will also prevent
-    // us from creating cyclic graphs.
-    if (child->second.parent != &parent->second)
-      throw std::runtime_error("child cannot have multiple parents");
-  }
+  // get the iterator to the child-node.
+  auto child = emplace(_tf.header.frame_id, _tf.child_frame_id, _static);
+  const Eigen::Isometry3d tf = tf2::transformToEigen(_tf);
 
   if (_static)
-    std::get<0>(child->second.data).set(tf2::transformToEigen(_tf));
-  else
-    std::get<1>(child->second.data)
-        .set(to_time(_tf.header.stamp), tf2::transformToEigen(_tf));
+    std::get<0>(child->second.data).set(tf);
+  else {
+    // dt is the dynamic transform
+    auto& dt = std::get<1>(child->second.data);
+    dt.set(to_time(_tf.header.stamp), tf);
+    // check if there is someone waiting for this transform
+    if (dt)
+      cv_.notify_all();
+  }
 }
 
 geometry_msgs::TransformStamped
 transform_tree::get(const std::string& _target, const std::string& _source,
                     const ros::Time& _time, const ros::Duration& _tolerance) {
   // tolerance cannot be negative
-  assert(_tolerance >= ros::Duration(0) && "tolerance cannot be negative");
+  std::unique_lock<std::mutex> l(m_);
 
   // get the iterators to the source and target frames
   auto s_iter = data_.find(_source);
   if (s_iter == data_.end())
-    throw std::runtime_error("unknown source frame");
+    throw std::runtime_error("unknown source frame " + _source);
 
   auto t_iter = data_.find(_target);
   if (t_iter == data_.end())
-    throw std::runtime_error("unknown target frame");
+    throw std::runtime_error("unknown target frame " + _target);
 
   const _leaf* source = &s_iter->second;
   const _leaf* target = &t_iter->second;
@@ -239,12 +333,18 @@ transform_tree::get(const std::string& _target, const std::string& _source,
   Eigen::Isometry3d source_root(Eigen::Isometry3d::Identity());
   Eigen::Isometry3d target_root(Eigen::Isometry3d::Identity());
   // convert to std::chrono
-  const time_t time(to_time(_time));
+  const time_t query_time(to_time(_time));
+  const duration_t tol(to_duration(_tolerance));
+  if (tol < duration_t{})
+    throw std::runtime_error("tolerance cannot be negative");
+
+  const auto end_time = clock_t::now() + tol;
 
   while (target->depth < source->depth) {
     // get the transformation of the source branch for the current depth and
     // advance to the next stage.
-    source_root = chain_transform(source->data, source_root, time);
+    source_root =
+        get_transform(source->data, query_time, end_time, l) * source_root;
     source = source->parent;
     assert(source && "tree holds a nullptr");
   }
@@ -254,10 +354,12 @@ transform_tree::get(const std::string& _target, const std::string& _source,
 
   // now walk up with both
   while (target->parent != source->parent) {
-    source_root = chain_transform(source->data, source_root, time);
+    source_root =
+        get_transform(source->data, query_time, end_time, l) * source_root;
     source = source->parent;
 
-    target_root = chain_transform(target->data, target_root, time);
+    target_root =
+        get_transform(target->data, query_time, end_time, l) * target_root;
     target = target->parent;
 
     assert(source && "tree holds a nullptr");
@@ -273,36 +375,43 @@ transform_tree::get(const std::string& _target, const std::string& _source,
   return msg;
 }
 
-transform_tree::impl_t::iterator
-transform_tree::init(const geometry_msgs::TransformStamped& _tf) {
-  if (!data_.empty()) {
-    // check if the child-frame-id is the current root. if not - we must reject
-    // the init-call since we already have a valid root-node.
-    auto maybe_root = data_.find(_tf.child_frame_id);
-    if (maybe_root == data_.end() || maybe_root->second.depth != 0)
-      throw std::runtime_error("cannot reinit the map");
-
-    // reinit the map's depth counters
-    for (auto& n : data_)
-      ++n.second.depth;
-    // create the root node and wire up the connections
-    auto new_root = init_root(_tf);
-    maybe_root->second.parent = &new_root->second;
-    return new_root;
-  }
-  else
-    return init_root(_tf);
+Eigen::Isometry3d
+transform_tree::get_transform(const dynamic_transform& _dyn_tr,
+                              const time_t& _query_time,
+                              const time_t& _end_time,
+                              std::unique_lock<std::mutex>& _lock) {
+  counter::scoped_signal signal{_dyn_tr};
+  do {
+    try {
+      return _dyn_tr.get(_query_time);
+    }
+    catch (const std::runtime_error& _ex) {
+      ROS_DEBUG_STREAM_THROTTLE(1, "failed to get the transform");
+    }
+  } while (cv_.wait_until(_lock, _end_time) != std::cv_status::timeout);
+  throw std::runtime_error("no transform available");
 }
 
-transform_tree::impl_t::iterator
-transform_tree::init_root(const geometry_msgs::TransformStamped& _tf) noexcept {
-  // create the root node
-  const auto res = data_.emplace(
-      _tf.header.frame_id, _leaf{_tf.header.frame_id, sequence_factory(true)});
-  assert(res.second && "failed to create the root node");
-  return res.first;
+Eigen::Isometry3d
+transform_tree::get_transform(const variable_transform& _var_tr,
+                              const time_t& _query_time,
+                              const time_t& _end_time,
+                              std::unique_lock<std::mutex>& _lock) {
+  if (_var_tr.index() == 0)
+    return std::get<0>(_var_tr).get();
+  else
+    return get_transform(std::get<1>(_var_tr), _query_time, _end_time, _lock);
 }
 
 }  // namespace detail
+
+void
+transform_buffer::set(const geometry_msgs::TransformStamped& _tf,
+                      bool _stamped) {}
+
+geometry_msgs::TransformStamped
+transform_buffer::get(const std::string& _target, const std::string& _source,
+                      const ros::Time& _time, const ros::Duration& _tolerance) {
+}
 
 }  // namespace fast_tf
--- src/fast_tf/fast_tf.hpp
+++ src/fast_tf/fast_tf.hpp
@@ -14,8 +14,10 @@ class Time;
 
 // std-lib
 #include <chrono>
+#include <condition_variable>
 #include <map>
 #include <memory>
+#include <mutex>
 #include <string>
 #include <string_view>
 #include <unordered_map>
@@ -24,10 +26,10 @@ class Time;
 namespace fast_tf {
 namespace detail {
 
-// below the definition of two transform types: static and dynamic. static
+// below the definition of two transform types: static and dynamic: static
 // transforms are time-independent; dynamic transforms store a sequence of
 // transforms. both classes correspont to the tf2::StaticCache and
-// tf2::TimedCache, rectively.
+// tf2::TimedCache, respectively.
 
 /// @brief Storage for a static transform.
 ///
@@ -58,12 +60,51 @@ using clock_t = std::chrono::system_clock;
 using time_t = std::chrono::time_point<clock_t>;
 using duration_t = std::chrono::nanoseconds;
 
+/**
+ * @brief Class with a scoped counter. The user can count up as long as the
+ * scoped_signal remains in the scope. The scoped_signal will count down as soon
+ * as it goes out of scope. The class is not thread-safe.
+ *
+ * Example
+ * @code
+ * // create an instance of the counter
+ * counter c;
+ *
+ * // create a scoped counter. counter is now at 1
+ * scoped_handle sc1{c};
+ *
+ * {
+ *  scoped_handle sc2{c}; // counter is now at 2
+ *  scoped_handle sc3{c}; // counter is now at 3
+ * }
+ * // counter is now back to 1
+ * @endcode
+ */
+struct counter {
+  /// @brief When constructed, the class will increment the count and decrement
+  /// when destructed.
+  struct scoped_signal {
+    scoped_signal(const counter& _c);
+    ~scoped_signal();
+
+  private:
+    size_t& counter_;
+  };
+
+  friend scoped_signal;
+
+  explicit operator bool() const noexcept { return counter_ != 0; }
+
+private:
+  mutable size_t counter_ = 0;
+};
+
 /// @brief Storage for a sequence of dynamic transforms.
 ///
 /// Class stores a timed sequence of a dynamically changing transforms. The
 /// implementation matches the implementation of the tf2::TimedCache. Similar to
 /// its static counterpart, the class is not thread-safe.
-struct dynamic_transform {
+struct dynamic_transform : public counter {
   /// @brief c'tor with the storage duration
   /// @param _dur positive duration
   explicit dynamic_transform(const duration_t& _dur) noexcept;
@@ -99,9 +140,86 @@ private:
 // interfaces.
 using variable_transform = std::variant<static_transform, dynamic_transform>;
 
+/// @brief Structure holding a transformation tree. The tree is a cycle-free
+/// connected chain, where every leaf has one parent (but the root), and an
+/// arbitrary number of children.
+struct _transform_tree {
+  /// @brief Structure holding the transform
+  ///
+  /// the structure implements a single-directed list where every node has one
+  /// parent but might have multiple leaves. the data to parent is owned
+  /// outside.
+  struct _leaf {
+    // constructors which will set depth and parent automatically
+    _leaf(const std::string& _name, variable_transform&& _data) noexcept;
+    _leaf(const std::string&, const _leaf& _parent,
+          variable_transform&& _data) noexcept;
+
+    std::string frame_id;    ///< the name of the leaf
+    unsigned int depth = 0;  ///< the current depth (increasing from root)
+    const _leaf* parent = nullptr;  ///< link to the parent; we don't own it
+    variable_transform data;        ///< the actual data; we own it
+  };
+
+  // the data structure
+  using impl_t = std::unordered_map<std::string, _leaf>;
+
+  // the iterators
+  using iterator = impl_t::iterator;
+  using const_iterator = impl_t::const_iterator;
+
+  // key-value types
+  using value_type = impl_t::value_type;  // _leaf
+  using key_type = impl_t::key_type;      // std::string
+
+  /// @brief Returns true, if the tree holds no data.
+  bool
+  empty() const noexcept {
+    assert(data_.end() == root_ && "root must point to the end");
+    return data_.empty();
+  }
+
+  /// @brief Initializes the new root node.
+  ///
+  /// A new root node can be created if the current tree is empty or if the
+  /// current root node is the direct child of the new root node: Adding the
+  /// link {X->A} to the tree {A->B->C} would then generate the tree
+  /// {X->A->B->C}.
+  ///
+  /// @return iterator to the new root node.
+  /// @throw std::runtime_error if new root node can't be inserted in the tree.
+  [[nodiscard]] iterator
+  init(const std::string& _parent_frame, const std::string& _child_frame);
+
+  /// @brief Returns the iterator to the node identified by _child_frame.
+  ///
+  /// The function will create a new node, if the node does not exist. It may
+  /// also create a new root node, if the parent node does not exist (see init).
+  ///
+  /// @throw std::runtime_error if a circle would be created, or if the tree
+  /// would be disconnected.
+  [[nodiscard]] iterator
+  emplace(const std::string& _parent_frame, const std::string& _child_frame,
+          bool _is_static);
+
+  /// @brief Merges the given tree into the current.
+  ///
+  /// The trees can only be merged if for at least one tree the common node is
+  /// the root node: {A->B->C} and {B->D} have the common node B and B is the
+  /// root node of the second tree; the resulting tree would be {A->B->{C, D}}.
+  /// The function is a noop, if one of the trees is empty.
+  ///
+  /// @throw std::runtime_error if the trees cannot be merged.
+  void
+  merge(_transform_tree& _other);
+
+  impl_t data_;
+  impl_t::iterator root_ = data_.end();
+};
+
 /// @brief structure holding the transform-tree data. the tree is connected and
 /// cycle-free (checked at insertion)
-struct transform_tree {
+struct transform_tree : private _transform_tree {
   /// @brief will add a new transformation to the tree.
   ///
   /// @param _tf the transform data
@@ -120,39 +238,36 @@ struct transform_tree {
   get(const std::string& _target, const std::string& _source,
       const ros::Time& _time, const ros::Duration& _tolerance);
 
+  bool
+  exists(const std::string& _frame) const noexcept {
+    return data_.find(_frame) != data_.end();
+  }
+
   // todo test me
   // todo add merge
   // todo add way for possibly connecting the trees
 private:
-  /// @brief leaf of the tree.
-  ///
-  /// the structure implements a single-directed list where every node has one
-  /// parent but might have multiple leaves. the data to parent is owned
-  /// outside.
-  struct _leaf {
-    // constructors which will set depth and parent automatically
-    _leaf(const std::string& _name, variable_transform&& _data) noexcept;
-    _leaf(const std::string&, const _leaf& _parent,
-          variable_transform&& _data) noexcept;
+  Eigen::Isometry3d
+  get_transform(const dynamic_transform& _dyn_tr, const time_t& _query_time,
+                const time_t& _end_time, std::unique_lock<std::mutex>& _lock);
 
-    std::string frame_id;    ///< the name of the leaf
-    unsigned int depth = 0;  ///< the current depth (increasing from root)
-    const _leaf* parent = nullptr;  ///< link to the parent; we don't own it
-    variable_transform data;        ///< the actual data; we own it
-  };
+  Eigen::Isometry3d
+  get_transform(const variable_transform& _var_tr, const time_t& _query_time,
+                const time_t& _end_time, std::unique_lock<std::mutex>& _lock);
 
-  // this is the owning part of the memory.
-  using impl_t = std::unordered_map<std::string, _leaf>;
-
-  [[nodiscard]] impl_t::iterator
-  init(const geometry_msgs::TransformStamped& _tf);
-
-  [[nodiscard]] impl_t::iterator
-  init_root(const geometry_msgs::TransformStamped& _tf) noexcept;
-
-  impl_t data_;
+  std::mutex m_;
+  std::condition_variable cv_;
 };
 }  // namespace detail
+
+struct transform_buffer {
+  void
+  set(const geometry_msgs::TransformStamped& _tf, bool _stamped);
+
+  geometry_msgs::TransformStamped
+  get(const std::string& _target, const std::string& _source,
+      const ros::Time& _time, const ros::Duration& _tolerance);
+};
 }  // namespace fast_tf
 
 #endif  // FAST_TF_FAST_TF_HPP__
